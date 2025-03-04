8625395ac682fc69ea462eac5e216016cb8f9b83
Vanessa Bienias <s30644@pjwstk.edu.pl>
2025-03-04 15:09:16 +0100

    Zadanie 3 (tablica intow)

--- Cwiczenia1/Cwiczenia1/Program.cs
+++ Cwiczenia1/Cwiczenia1/Program.cs
@@ -1,2 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("H");
+
+static double GetAverage(int[] arr)
+{
+    return 0;
+}
+
+int[] arr = { 1, 2, 3, 4, 5 };
+GetAverage(arr);
